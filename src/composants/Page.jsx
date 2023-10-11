5adf1279857800ad0e7324f96ffb7082e129e946
harry92300 <133029394+harry92300@users.noreply.github.com>
2023-10-11 14:27:02 +0200

    bjr

---
+++ src/composants/Page.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import '../style/page.css'
+
+
+
+
+export default function page (){
+
+
+    <div className="maison">
+        <link rel="stylesheet" href="" />
+
+<h1>btfgh</h1>
+    </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
